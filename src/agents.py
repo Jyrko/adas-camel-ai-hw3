da8c5885dc73f28601aac8cdc4772a62272909bf
Jyrko <uriydemoshenko@gmail.com>
2025-06-06 12:15:52 +0200

    add workforce

--- src/agents.py
+++ src/agents.py
@@ -40,15 +40,23 @@ class SampleAgent(ChatAgent):
 
 class PreferenceAgent(ChatAgent):
   """
-    A preference agent, which collects and stores the preferences of the user regarding jobs
+    A preference agent, which collects (asks) and stores the preferences of the user regarding jobs
   """
   agent_role = BaseMessage.make_assistant_message(
     role_name="preference_agent",
-    content="You are a preference agent, which collects and stores the preferences of the user regarding jobs."
+    content="You are a preference agent, which collects (asks) and stores the preferences of the user regarding jobs."
   )
 
+  human_toolkit = HumanToolkit()
+
   def __init__(self, model, message_window_size: int = 20):
-    super().__init__(model=model, system_message=self.agent_role, message_window_size=message_window_size)
+
+    super().__init__(
+      model=model, 
+      system_message=self.agent_role, 
+      message_window_size=message_window_size,
+      tools=[*self.human_toolkit.get_tools()]
+    )
 
 class JobSearchAgent(ChatAgent):
   """
